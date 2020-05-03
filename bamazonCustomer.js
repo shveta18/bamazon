8b7b182f0fe30fcf531124ef21b59b7df0e97525
Shveta Kumar <sk@shveta.ca>
2020-05-03 17:30:37 -0400

    stored all product details into a product array. Replaced console table with console log of items. Added prompts for product purchase decision.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,27 +8,69 @@ var connection = mysql.createConnection({
     password: "junket-serrate-postdate",
     database: "bamazon_db"
 });
-
-connection.connect(function(err) {
+// once connection is established, run function to list all products
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     productList();
 });
 
+var products = [];
+// function to display all product list, prices and ids
 function productList() {
     var productList = "SELECT * FROM products;";
-    console.log("List of Products for purchase: ");
-    connection.query(productList, function(err, res) {
+    console.log("================================ LIST OF PRODUCTS IN OUR STORE ===============================");
+    connection.query(productList, function (err, res) {
         if (err) throw err;
-        var products = {};
-        console.table(res, ["item_id", "product_name", "price"]);
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: (" + res[i].item_id + ") " + res[i].product_name + " || $" + res[i].price);
+            // push all results to the products array
+            products.push(res[i]);
+        }
+        // run function for product selection
+        purchaseDecision();
     });
+}
+// purchase decision
+function purchaseDecision() {
+    inquirer
+        .prompt({
+            name: "purchase decision",
+            type: "list",
+            message: "Would you like to make a purchase?",
+            choices: ["Yes", "No", "exit"]
+        })
+        .then(function (answer) {
+            switch (answer["purchase decision"]) {
+                case "Yes":
+                    break;
+
+                case "No":
+                    noPurchase();
+                    connection.end()
+                    break;
+
+                case "exit":
+                    noPurchase();
+                    connection.end();
+                    break;
+            }
+        });
+}
+// if user selects no, or, exits  message displays to exit
+function noPurchase() {
+    console.log("Thank you for visiting the store! Come back soon!");
 }
 
 function productSelect() {
     inquirer
-    .prompt([
-        {}
-    ])
-}
+        .prompt({
+            name: "select product ID",
+            type: "list",
+            message: "select the ID of the product you would like to purchase",
+            choices: [
 
+            ]
+
+        })
+}
